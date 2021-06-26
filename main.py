afd27d4643605d6d230e02d200e6c253672db985
baronoflulz <baronoflulz@gmail.com>
2021-06-26 23:08:09 +0300

    Finished basic workings of results page

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect,url_for
+from flask import Flask, render_template, request, redirect,url_for, Response
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 import pickle
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -15,7 +19,7 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/analyzeWine', methods =["POST","GET"])
+@app.route('/analyzeWine', methods=["POST","GET"])
 def analyze_wine():
     if request.method == "POST":
         fixed_acidity = request.form["fa"]
@@ -28,16 +32,34 @@ def analyze_wine():
         ph = request.form["pH"]
         sulphates = request.form["su"]
         alcohol = request.form["alc"]
-        vec =  np.array([fixed_acidity,volatile_acidity,citric_acid,residual_sugar,
-                         chlorides,free_sulfur_dioxide, total_sulfur_dioxide, ph,sulphates,
+        vec =  np.array([fixed_acidity, volatile_acidity, citric_acid, residual_sugar,
+                         chlorides, free_sulfur_dioxide, total_sulfur_dioxide, ph, sulphates,
                          alcohol]).reshape(-1,10)
         result = rf_classifier.predict_proba(vec)
         return redirect(url_for("results", prob_yes=result[0][0], prob_no=result[0][1]))
     else:
         return render_template('AnalyzeWine.html')
 @app.route('/results/<float:prob_yes>/<float:prob_no>')
-def results(prob_yes,prob_no):
-    return 'working on it'
+def results(prob_yes, prob_no):
+    pos = prob_yes
+    neg = prob_no
+    fig = create_figure(pos,neg)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(output.getvalue()).decode('utf8')
+
+
+    return render_template("results.html",image=pngImageB64String)
+
+def create_figure(pos,neg):
+    fig = Figure(facecolor=(0.2,0.2,0.2))
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_facecolor((0.2,0.2,0.2))
+    axis.bar(["Probability: rated well", "Probability: rated Badly"],
+    [pos, neg], width=0.5, color=(0.5,0,0))
+    return fig
+
 
 if __name__ == '__main__':
     app.run()
